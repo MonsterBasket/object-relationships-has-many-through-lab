e21994a8745dbe10643bd3d712b58d648bfcd536
MonsterBasket <blazza1@hotmail.com>
2022-09-16 17:07:26 +1000

    done with the songs

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,27 @@
+class Artist
+  attr_accessor :name
+  @@all = []
 
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select { |s| s.artist.name == name }
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+    genres = []
+    self.songs.select{ |s| genres << s.genre }
+    genres.uniq
+  end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,23 @@
+class Genre
+  attr_accessor :name
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select { |s| s.genre = name }
+  end
+
+  def artists
+    artists = []
+    self.songs.map { |s| artists << s.artist }
+    artists.uniq
+  end
+end
